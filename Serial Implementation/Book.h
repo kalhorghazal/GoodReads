fc1d8a3dc766535a099a84387cf1f4204cdada9f
Ghazal <kalhorghazal1378@gmail.com>
2020-04-20 14:48:36 +0000

    Add methods for calculating Rating

--- Serial Implementation/Book.h
+++ Serial Implementation/Book.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <iomanip>
 
 #define NEW_LINE '\n'
 #define ZERO 0
@@ -11,11 +12,16 @@
 #define SPACE ' '
 #define COLON ':'
 #define COMMA ','
-#define ID "id"
-#define TITLE "Title"
+#define FALSE false
+#define TRUE true
+//#define ID "id"
+//#define TITLE "Title"
 #define GENRES "Genres"
 #define NUMBER_OF_PAGES "Number of Pages"
 #define AUTHOR "Author"
+#define AVERAGE_RATING "Averager Rating"
+#define PRECISION 2
+#define RATING_COEFFICIENT 1/10
 
 //Type Defines
 
@@ -25,13 +31,17 @@ class Book
 {
 public:
 	Book(int _id, string _title, string _genre_1, string _genre_2, 
-		string _author_name, int _pages, float _author_average_rating);
+		int _pages, string _author_name, float _author_average_rating);
 	int get_id() const { return id; }
     string get_title() const { return title; }
     string get_genre_1() const { return genre_1; }
     string get_genre_2() const { return genre_2; }
     string get_author_name() const { return author_name; }
     int get_pages() const { return pages; }
+    float get_average_rating() const { return average_rating; }
+    void update_rating(int rating, int number_of_likes);
+    void calulate_average_rating();
+    bool has_genre(string genre);
 private:
 	int id; 
 	string title;
@@ -40,7 +50,8 @@ private:
 	string author_name;
 	int pages; 
 	float author_average_rating;
-	float average_rating; // ????????
+	float average_rating; 
+	int number_of_reviews;
 
 	friend ostream& operator<<(ostream&, const Book&);
 };
