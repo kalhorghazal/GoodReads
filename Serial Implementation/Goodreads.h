b17a088640e44f3ef1fd1fa6f141f49ee765d25f
Ghazal <kalhorghazal1378@gmail.com>
2020-04-20 14:54:57 +0000

    Using Map Container and processing Datasets

--- Serial Implementation/Goodreads.h
+++ Serial Implementation/Goodreads.h
@@ -6,16 +6,18 @@
 #include <sstream>
 #include <vector>
 #include <string>
-#include <iomanip>
+#include <map>
 
 #define NEW_LINE '\n'
-#define BOOKS "books"
-#define REVIEWS "reviews"
-#define SUFFIX ".csv"
+#define BOOKS_FILE "books.csv"
+#define REVIEWS_FILE "reviews.csv"
 #define GENRE 1
+#define BOOK_NOT_FOUND -1
 
 using namespace std;
 
+typedef int Book_id;
+typedef std::map<Book_id, Book*> _Books;
 typedef vector<Book*> Books;
 typedef vector<Review*> Reviews;
 
@@ -37,7 +39,11 @@ enum Review_feature
 	NUMBER_OF_LIKES
 };
 
-Book* get_new_book_info(string line);
+//Book* get_new_book_info(string line);
+void get_new_book_info(_Books& books, string line);
 Review* get_new_review_info(string line);
-void read_csv(Books& books, string filename);
+void read_csv(_Books& books, string filename);
 void read_csv(Reviews& reviews, string filename);
+void count_ratings(Reviews reviews, _Books& books, string genre);
+void find_best_book(_Books& books, string genre);
+//int find_book_index_by_id(Books books, Book_id id);
