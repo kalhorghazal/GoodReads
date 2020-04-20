d7618feb2656c4beeff1d18e98aab3e19e570045
Ghazal <kalhorghazal1378@gmail.com>
2020-04-20 17:03:36 +0000

    Refactoring

--- Serial Implementation/Book.h
+++ Serial Implementation/Book.h
@@ -6,52 +6,51 @@
 #include <vector>
 #include <iomanip>
 
-#define NEW_LINE '\n'
-#define ZERO 0
-#define ONE 1
 #define SPACE ' '
 #define COLON ':'
 #define COMMA ','
-#define FALSE false
+#define NEW_LINE '\n'
 #define TRUE true
-//#define ID "id"
-//#define TITLE "Title"
-#define GENRES "Genres"
-#define NUMBER_OF_PAGES "Number of Pages"
-#define AUTHOR "Author"
-#define AVERAGE_RATING "Average Rating"
+#define FALSE false
+#define ZERO 0
 #define PRECISION 2
 #define RATING_RATIO 0.1
+#define ID_STR "id"
+#define TITLE_STR "Title"
+#define GENRES_STR "Genres"
+#define AUTHOR_STR "Author"
+#define AVERAGE_RATING_STR "Average Rating"
+#define NUMBER_OF_PAGES_STR "Number of Pages"
 
-//Type Defines
+typedef int Book_id;
 
 using namespace std;
 
 class Book
 {
 public:
-	Book(int _id, string _title, string _genre_1, string _genre_2, 
-		int _pages, string _author_name, float _author_average_rating);
-	int get_id() const { return id; }
+	Book(Book_id _id, string _title, string _genre_1, string _genre_2, 
+		int _pages, string _author_name, double _author_average_rating);
+	Book_id get_id() const { return id; }
     string get_title() const { return title; }
     string get_genre_1() const { return genre_1; }
     string get_genre_2() const { return genre_2; }
     string get_author_name() const { return author_name; }
     int get_pages() const { return pages; }
-    float get_average_rating() const { return average_rating; }
+    double get_average_rating() const { return average_rating; }
     void update_rating(int rating, int number_of_likes);
     void calulate_average_rating();
     bool has_genre(string genre);
 private:
-	int id; 
+	Book_id id; 
 	string title;
 	string genre_1;
 	string genre_2;
 	string author_name;
 	int pages; 
-	float author_average_rating;
-	float average_rating; 
 	int total_number_of_likes;
+	double author_average_rating;
+	double average_rating; 
 
 	friend ostream& operator<<(ostream&, const Book&);
 };
