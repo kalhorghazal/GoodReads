5662cee5ba04726fc82e3f5ed77ee3d39be79dfb
Ghazal <kalhorghazal1378@gmail.com>
2020-04-22 10:33:04 +0000

    Refactoring

--- Bonus Implementation/Goodreads.cpp
+++ Bonus Implementation/Goodreads.cpp
@@ -10,6 +10,7 @@
 #include <map>
 
 #define NEW_LINE '\n'
+#define ONE 1
 #define ZERO 0
 #define GENRE 1
 #define NUMBER_OF_BOOK_THREADS 4
@@ -20,8 +21,7 @@
 using namespace std;
 
 typedef int Book_id;
-typedef std::map<Book_id, Book*> _Books;
-typedef vector<Book*> Books;
+typedef map<Book_id, Book*> Books;
 typedef vector<Review*> Reviews;
 
 enum Book_feature
@@ -48,19 +48,17 @@ typedef struct
 	int length; 
 } Read_data;
 
-_Books books;
+Books books;
 Reviews reviews;
 pthread_t book_threads[NUMBER_OF_BOOK_THREADS];
 pthread_t review_threads[NUMBER_OF_REVIEW_THREADS];
 pthread_mutex_t mutex_read_book;
 pthread_mutex_t mutex_read_review;
 
-void get_new_book_info(_Books& books, string line);
 Review* get_new_review_info(string line);
-void read_csv(_Books& books, string filename);
-void read_csv(Reviews& reviews, string filename);
-void count_ratings(Reviews reviews, _Books& books, string genre);
-void find_best_book(_Books& books, string genre);
+void get_new_book_info(Books& books, string line);
+void count_ratings(Reviews reviews, Books& books, string genre);
+void find_best_book(Books& books, string genre);
 int get_file_length(string filename);
 void* read_books(void* arg);
 void* read_reviews(void* arg);
