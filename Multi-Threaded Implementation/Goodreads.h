8621a95bf77bedaefacaa87d2b461df054988bef
Ghazal <kalhorghazal1378@gmail.com>
2020-04-24 11:08:02 +0000

    Refactoring

--- Multi-Threaded Implementation/Goodreads.h
+++ Multi-Threaded Implementation/Goodreads.h
@@ -1,29 +1,30 @@
 #include "Book.h"
 #include "Review.h"
 
-#include <iostream>
-#include <fstream>
-#include <sstream>
+#include <map>
+#include <omp.h>
 #include <vector>
 #include <string>
-#include <map>
+#include <fstream>
+#include <sstream>
+#include <iostream>
 #include <pthread.h>
 #include <algorithm>
 
-#define NEW_LINE '\n'
 #define ZERO 0
 #define GENRE 1
-#define NUMBER_OF_BOOK_THREADS 4
-#define NUMBER_OF_REVIEW_THREADS 6
 #define SUFFIX ".csv"
+#define NEW_LINE '\n'
 #define BOOKS "Assets/books_"
 #define REVIEWS "Assets/reviews_"
+#define NUMBER_OF_BOOK_THREADS 4
+#define NUMBER_OF_REVIEW_THREADS 6
 
 using namespace std;
 
 typedef int Book_id;
-typedef map<Book_id, Book*> Books;
 typedef vector<Review*> Reviews;
+typedef map<Book_id, Book*> Books;
 
 enum Book_feature
 {
@@ -45,16 +46,18 @@ enum Review_feature
 
 Books books;
 Reviews reviews;
-pthread_t book_threads[NUMBER_OF_BOOK_THREADS];
-pthread_t review_threads[NUMBER_OF_REVIEW_THREADS];
 pthread_mutex_t mutex_read_book;
 pthread_mutex_t mutex_read_review;
+pthread_t book_threads[NUMBER_OF_BOOK_THREADS];
+pthread_t review_threads[NUMBER_OF_REVIEW_THREADS];
 
-Review* get_new_review_info(string line);
-void get_new_book_info(Books& books, string line);
-void read_csv(Books& books, string filename);
-void read_csv(Reviews& reviews, string filename);
-void count_ratings(Reviews reviews, Books& books, string genre);
-void find_best_book(Books& books, string genre);
+void call_book_readers();
+void call_review_readers();
 void* read_books(void* arg);
 void* read_reviews(void* arg);
+Review* get_new_review_info(string line);
+void read_csv(Books& books, string filename);
+void find_best_book(Books& books, string genre);
+void read_csv(Reviews& reviews, string filename);
+void get_new_book_info(Books& books, string line);
+void count_ratings(Reviews reviews, Books& books, string genre);
