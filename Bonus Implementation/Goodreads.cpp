c8dca055067f35078c6ae5d53f9f1adfde679241
Ghazal <kalhorghazal1378@gmail.com>
2020-04-25 18:39:21 +0000

    Optimizing

--- Bonus Implementation/Goodreads.cpp
+++ Bonus Implementation/Goodreads.cpp
@@ -2,13 +2,13 @@
 
 int main(int argc, char const *argv[])
 {
-	string genre = argv[GENRE];
+	genre = argv[GENRE];
 
 	call_book_readers();
 	call_review_readers();
 	
-	count_ratings(reviews, books, genre);
-	find_best_book(books, genre);
+	count_ratings(reviews, books);
+	find_best_book(books);
 
 	pthread_exit(NULL);
 }
@@ -79,10 +79,11 @@ void extract_new_book_info(Books& books, string line)
 		info[GENRE_2], atoi(info[PAGES].c_str()), info[AUTHOR_NAME], 
 		atof(info[AUTHOR_AVERAGE_RATING].c_str()));
 
-	books[atoi(info[ID].c_str())] = book;
+	if (book->has_genre(genre))
+		books[atoi(info[ID].c_str())] = book;
 }
 
-Review* get_new_review_info(string line)
+void extract_new_review_info(Reviews& reviews, string line)
 {
 	istringstream templine(line);
 	string data;
@@ -95,42 +96,39 @@ Review* get_new_review_info(string line)
 	Review* review = new Review(atoi(info[BOOK_ID].c_str()), 
 		atoi(info[RATING].c_str()), atoi(info[NUMBER_OF_LIKES].c_str()));
 
-	return review;
+	if (contains_key(atoi(info[BOOK_ID].c_str())))
+		reviews.push_back(review);
 }
 
-void count_ratings(Reviews reviews, Books& books, string genre)
+void count_ratings(Reviews reviews, Books& books)
 {
-    #pragma omp parallel for
+    #pragma omp parallel for 
 	for (int i = ZERO; i < reviews.size(); ++i)
 	{
 		Book_id book_id = reviews[i]->get_book_id();
 
-		if (books[book_id]->has_genre(genre))
-			books[book_id]->update_rating(reviews[i]->get_rating(), 
-				reviews[i]->get_number_of_likes());
+		books[book_id]->update_rating(reviews[i]->get_rating(), 
+			reviews[i]->get_number_of_likes());
 	}
 }
 
-void find_best_book(Books& books, string genre)
+void find_best_book(Books& books)
 {
 	Book* best;
 	double max_rating = ZERO;
 	
-	#pragma omp parallel for
+	#pragma omp parallel for 
 	for (Books::iterator it = books.begin(); 
 		it != books.end(); ++it)
 	{
-		Book* book = it->second;
-		
-		if (book->has_genre(genre))
-		{
-    	 	book->calulate_average_rating();
-    	 	if (book->get_average_rating() > max_rating)
-    	 	{
-    	 		best = book;
-    	 		max_rating = book->get_average_rating();
-    	 	}
-    	 }
+		Book* book = it->second;		
+    	book->calulate_average_rating();
+
+    	if (book->get_average_rating() > max_rating)
+    	{
+    		best = book;
+    	 	max_rating = book->get_average_rating();
+    	}
 	}
 
 	cout << *best;
@@ -144,6 +142,14 @@ int get_file_length(string filename)
 
 	return length;
 
+}
+
+bool contains_key(int key)
+{
+	if (books.find(key) != books.end())
+		return TRUE;
+
+	return FALSE;
 }
 
 void* read_books(void* arg)
@@ -191,7 +197,7 @@ void* read_reviews(void* arg)
 
 	while (getline(file, line, NEW_LINE))
 	{
-		my_reviews.push_back(get_new_review_info(line));
+		extract_new_review_info(my_reviews, line);
 		if (file.tellg() > (read_data->start + read_data->length))
 			break;
 	}
